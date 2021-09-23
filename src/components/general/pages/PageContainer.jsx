6945e74d119f32e92e3289d3a5007264c64c7e73
Andrew P <apatinoc4@gmail.com>
2021-09-22 22:22:15 -0500

    Component reestructuring based on projects

---
+++ src/components/general/pages/PageContainer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Footer from "../templates/footer/footer";
+import Header from "../templates/header/header";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import MainIndex from "../../index_homepage/pages/main-index/mainIndex";
+import ReportTool from "../../account_review_tool/pages/reportTool";
+import UnitPlanner from "../../unit_planner/pages/unit-planner/unit-planner";
+
+library.add(fab);
+
+const PageContainer = () => {
+  return (
+    <div>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={MainIndex} />
+          <Route path="/review_tool" exact component={ReportTool} />
+          <Route path="/unit_planner" exact component={UnitPlanner} />
+        </Switch>
+        <Footer />
+      </Router>
+    </div>
+  );
+};
+
+export default PageContainer;

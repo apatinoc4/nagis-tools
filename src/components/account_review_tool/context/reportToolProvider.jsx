9e7a47af25560f87846a7dc857fd131d5b03c698
Andres Patino <apatinoc4@gmail.com>
2022-02-22 19:27:58 -0500

    updated values

--- src/components/account_review_tool/context/reportToolProvider.jsx
+++ src/components/account_review_tool/context/reportToolProvider.jsx
@@ -8,17 +8,17 @@ const ReportProvider = ({ children }) => {
   // Values for minimum requirements and amazing requirements
 
   const amazing = {
-    units: activeGuild === "Krispy-Kreme" ? 25 : 6,
-    espers: activeGuild === "Krispy-Kreme" ? 20 : 5,
-    vcs: activeGuild === "Krispy-Kreme" ? 15 : 3,
-    gear: activeGuild === "Krispy-Kreme" ? 35 : 8,
+    units: activeGuild === "Krispy-Kreme" ? 32 : 6,
+    espers: activeGuild === "Krispy-Kreme" ? 23 : 5,
+    vcs: activeGuild === "Krispy-Kreme" ? 20 : 3,
+    gear: activeGuild === "Krispy-Kreme" ? 40 : 8,
   };
 
   const minimum = {
-    units: activeGuild === "Krispy-Kreme" ? 14 : 3,
-    espers: activeGuild === "Krispy-Kreme" ? 12 : 3,
-    vcs: activeGuild === "Krispy-Kreme" ? 7 : 1,
-    gear: activeGuild === "Krispy-Kreme" ? 18 : 4,
+    units: activeGuild === "Krispy-Kreme" ? 16 : 3,
+    espers: activeGuild === "Krispy-Kreme" ? 14 : 3,
+    vcs: activeGuild === "Krispy-Kreme" ? 10 : 1,
+    gear: activeGuild === "Krispy-Kreme" ? 20 : 4,
   };
   return (
     <ReportContext.Provider
